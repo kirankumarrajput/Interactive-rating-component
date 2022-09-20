5ba56970c8aeb6cdac60d48b0d0c0b7e71e6256e
kiran kumar <112935491+kirankumarrajput@users.noreply.github.com>
2022-09-20 22:35:25 +0530

    Interactive rating component
    Interactive rating component

---
+++ js.js
@@ -0,0 +1,23 @@
+const btn = document.querySelector(".btn");
+const ThankYouPage = document.querySelector(".thankyou");
+const ratings = document.querySelectorAll(".circle");
+const rating = document.querySelector("#rating");
+const firstCard = document.querySelector(".card");
+
+var rate = 0;
+
+for (let i = 0; i < ratings.length; i++) {
+  ratings[i].addEventListener("click", () => {
+    rate = ratings[i].innerText;
+
+    for (let j = 0; j < rate; j++) {
+      ratings[j].style.backgroundColor = "yellow";
+    }
+  });
+}
+
+btn.addEventListener("click", () => {
+  firstCard.style.display = "none";
+  rating.innerHTML = rate;
+  ThankYouPage.style.display = "block";
+});
